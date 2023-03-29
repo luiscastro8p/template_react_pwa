228a3a42e79e5359446b12fbb4fe99fe582dbf83
Luis Castro <luisarmandocastro12@gmail.com>
2023-03-28 20:11:45 -0700

    cambios en login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import './login.scss';
 
 const LoginView = () => {
-  return <div>Walkway</div>;
+  return (
+    <Row className="content-login">
+      <Col sm={12} md={6}>
+        Walkway
+        <Button> Guardar informacion</Button>
+      </Col>
+    </Row>
+  );
 };
 
 export default LoginView;
